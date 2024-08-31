be7352fac797b3be3c2b858ddf385d4f44c0bf3c
Dubjay18 <jejeniyi7@gmail.com>
2024-08-31 21:21:32 +0100

    feat: user model

---
+++ internal/models/user.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `gorm:"uniqueIndex;not null" json:"username"`
+	Password string `gorm:"not null" json:"-"`
+	Email    string `gorm:"uniqueIndex;not null" json:"email"`
+}
+
+// HashPassword hashes the user's password
+func (user *User) HashPassword(password string) error {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		return err
+	}
+	user.Password = string(bytes)
+	return nil
+}
+
+// CheckPassword checks if the provided password matches the stored password
+func (user *User) CheckPassword(providedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(providedPassword))
+}
